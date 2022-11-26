b49ca03cc3c9c749b130f31e301788918646eba4
Berlin Andrey <99011598+Andrey-Berlin@users.noreply.github.com>
2022-11-26 23:14:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-let mas =['1','2','3','4','5','6','7','8','9',
+let mas1 =['1','2','3','4','5','6','7','8','9',
 '10','11','12','13','14','15','16','17',
 '18','19','20','21','22','23','24',
 '25','26','27','28']
@@ -18,7 +18,7 @@ for (let i = 0; i < click.length; i ++) {
 
          let id = this.getAttribute("id");
 
-         if(id == mas[0]) {
+         if(id == mas1[0]) {
 
          document.location.href = "https://andrey-berlin.github.io/FrontEndBackEndResurs/";
 
